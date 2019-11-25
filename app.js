d17d5cc04d6e1d1ee6ae5a8f18badc0f28a33c1f
Anand Hegde <hegde.an@husky.neu.edu>
2019-11-24 22:40:32 -0500

    Adding studd

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,26 +18,32 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//configure auth here.
+app.use(express.session({ secret: 'chelsea sucks' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error pagensureIndexe
   res.status(err.status || 500);
   res.render('error');
 });
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => console.log('Error in DB connection : ' + err));
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('../models/user');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  userModel.find().then(data => res.send(data));
+});
+
+router.post('/', (req, res, next) => {
+  //Do some validation here.
+  const newUser = new userModel(req.body.user);
+  newUser.save()
+    .then(() => res.send('user saved'))
+    .catch(err => res.send({ status: 'failed', message: err }));
+});
+
+router.post('/login', (req, res, next) => {
+
 });
 
 module.exports = router;
