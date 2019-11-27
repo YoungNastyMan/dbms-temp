98a48079c53f2e5bb7f32531028789f575a615fc
swapnilblues <swapnilbhaumikcfc@gmail.com>
2019-11-26 21:20:01 -0500

    Added login

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET registration page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
+
+/*GET login succes page */
+router.get('/loginsuccess', function(req, res, next) {
+  console.log(req.user);
+  if(!req.user) {
+    res.render('loginFailure');
+  }
+  res.render('loginSuccess', { user: req.user });
+});
+
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,17 @@ var router = express.Router();
 const userModel = require('../models/user');
 const passport = require('passport');
 
-/* GET users listing. */
+/* GET user listing. */
 router.get('/', function (req, res, next) {
   if(!req.user) {
     res.send('No auth');
   }
-  userModel.find().then(data => res.send(data));
+  userModel.findById(req.user._id).then(data => res.send(data));
 });
 
-router.post('/', (req, res, next) => {
+
+
+router.post('/register', (req, res, next) => {
   //Do some validation here.
   const newUser = new userModel(req.body.user);
   newUser.save()
@@ -19,6 +21,8 @@ router.post('/', (req, res, next) => {
     .catch(err => res.send({ status: 'failed', message: err }));
 });
 
-router.post('/login', passport.authenticate('local', { successRedirect: '/users/' }));
+router.post('/useradded', passport.authenticate('local', { successRedirect: '/users/useradded' }));
+router.post('/login', passport.authenticate('local', { successRedirect: '/loginsuccess' }));
+
 
 module.exports = router;
