e94de74d873d1a515cce803d51a5f18b2aa2d4f7
swapnilblues <swapnilbhaumikcfc@gmail.com>
2019-11-27 23:09:08 -0500

    Need to submit json from register.ejs form

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var redirectRouter = require('./routes/redirect');
 
 var app = express();
 
@@ -64,6 +65,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/redirect', redirectRouter);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => console.log('Error in DB connection : ' + err));
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var querystring = require('querystring');
 const userModel = require('../models/user');
 const passport = require('passport');
-
+var request = require('request');
 /* GET user listing. */
 router.get('/', function (req, res, next) {
   console.log("here");
-  if(!req.user) {
+  if (!req.user) {
     res.send('No auth');
   }
   userModel.findById(req.user._id).then(data => res.send(data));
@@ -15,6 +16,11 @@ router.get('/', function (req, res, next) {
 //register new user
 router.post('/register', (req, res, next) => {
   //Do some validation here.
+  console.log("Users");
+  
+  console.log(req.body.user);
+
+
   const newUser = new userModel(req.body.user);
   newUser.save()
     .then(() => res.send('user saved'))
@@ -22,34 +28,72 @@ router.post('/register', (req, res, next) => {
 });
 
 //update user prfoile
-router.put('/updateUser',  (req, res, next)=>{
+router.put('/updateUser', (req, res, next) => {
   console.log("hi");
   console.log(req.body.user);
-   userModel.updateOne(
-    { username: req.body.user.username},
-    { $set:
-       {
-         first_name: req.body.user.first_name,
-         last_name: req.body.user.last_name,
-         password: req.body.user.password,
-         address: {
-           address : req.body.user.address.address,
-           pincode : req.body.user.address.pincode,
-           city  : req.body.user.address.city,
-           state : req.body.user.address.state,
-           country: req.body.user.address.country
-         }
+  userModel.updateOne(
+    { username: req.body.user.username },
+    {
+      $set:
+      {
+        first_name: req.body.user.first_name,
+        last_name: req.body.user.last_name,
+        password: req.body.user.password,
+        address: {
+          address: req.body.user.address.address,
+          pincode: req.body.user.address.pincode,
+          city: req.body.user.address.city,
+          state: req.body.user.address.state,
+          country: req.body.user.address.country
+        }
 
-       }
+      }
     }
- ) .then(() => res.send('user updated'))
- .catch(err => res.send({ status: 'failed to update', message: err }));
+  ).then(() => res.send('user updated'))
+    .catch(err => res.send({ status: 'failed to update', message: err }));
 
 })
 
+//temp register
+router.post('/register1', (req, res, next) => {
+  console.log("Redirect in Users");
+  console.log(req.body.username);
+  var j = {
+    'user': {
+      'first_name': req.body.first_name,
+      'last_name': req.body.first_name,
+      'username': req.body.last_name,
+      'password': req.body.username,
+      'usertype': req.body.usertype
 
+    }
+  }
+  console.log(j);
+
+  request.post({
+    url: "http://localhost:3000/users/register",
+
+    json: j
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(body)
+    }
+    else {
+
+      console.log("error: " + response)
+      console.log("response.statusCode: " + response.statusCode)
+      console.log("response.statusText: " + response.statusText)
+    }
+  }
+  )
+})
 
 router.post('/useradded', passport.authenticate('local', { successRedirect: '/users/useradded' }));
 
 
+
 module.exports = router;
+
+
+
+
